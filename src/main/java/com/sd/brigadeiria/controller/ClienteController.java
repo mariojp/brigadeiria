73371c5f61578c293de46d9fedcb397bd557311a
Patrícia <patydourado@gmail.com>
2021-11-26 08:22:14 -0300

    adicionado método lista clientes por tipo

--- src/main/java/com/sd/brigadeiria/controller/ClienteController.java
+++ src/main/java/com/sd/brigadeiria/controller/ClienteController.java
@@ -1,14 +1,12 @@
 package com.sd.brigadeiria.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
 import com.sd.brigadeiria.model.Cliente;
 import com.sd.brigadeiria.repository.ClienteRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -22,5 +20,15 @@ public class ClienteController {
         List<Cliente> lista = clienteRepository.findAll();
         return lista;
     }
+
+    /*Exemplo de passagem de parâmetro
+    Na url ?tipo= */
+    @GetMapping(value="/clientesportipo/" )
+    public List<Cliente> listaClientesPorTipo(@RequestParam(value="tipo") String tipo){
+        System.out.println("o tipo é" + tipo);        
+        List<Cliente> lista = clienteRepository.findByTipo(tipo);
+        return lista;
+    }
+
     
 }
--- src/main/java/com/sd/brigadeiria/repository/ClienteRepository.java
+++ src/main/java/com/sd/brigadeiria/repository/ClienteRepository.java
@@ -3,9 +3,12 @@ package com.sd.brigadeiria.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.sd.brigadeiria.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     
+    List<Cliente> findByTipo(String tipo);
 }
